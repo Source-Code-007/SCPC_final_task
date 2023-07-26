14ae1db38bf4bf7dda87ae4119f55b2994e0103b
Source-Code-007 <utsho926@gmail.com>
2023-07-26 17:10:05 +0600

    final retouch

--- SCPC_final_task.js
+++ SCPC_final_task.js
@@ -10,6 +10,7 @@ console.log(strReverseFunc('hello Bangladesh, welcome!'));
 
 
 
+
 // Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array. 
 const sumOfPositiveNum = (arr) => {
     let sum = 0
